1c7838bbf96472aee2d2f72b8368678629f2ed65
techwulf <t.zach.moore@gmail.com>
2021-08-18 16:11:49 -0600

    add form code

--- user-onboarding/src/components/UserForm.js
+++ user-onboarding/src/components/UserForm.js
@@ -7,11 +7,63 @@ export default function UserForm(props) {
     evt.preventDefault();
     submit();
   }
+  const onChange = evt => {
+    const {name, value, checked, type} = evt.target;
+    const valueToUse = type === 'checkbox' ? checked : value;
+    change(name, valueToUse);
+  }
   return (
 
     <div>
       <form onSubmit={onSubmit}>
-
+        <h2>New User</h2>
+        <div class="form-item">
+          <p class="error-message"></p>
+          <label>Username
+            <input 
+              value={values.username}
+              onChange={onChange}
+              placeholder='Enter a Username'
+              name='username'
+              type='text'
+            />
+          </label>
+        </div>
+        <div class="form-item">
+          <p class="error-message"></p>
+          <label>email
+            <input 
+              value={values.email}
+              onChange={onChange}
+              placeholder='Enter an Email'
+              name='email'
+              type='email'
+            />
+          </label>
+        </div>
+        <div class="form-item">
+          <p class="error-message"></p>
+          <label>Password
+            <input 
+              value={values.password}
+              onChange={onChange}
+              placeholder='Enter a Password'
+              name='password'
+              type='password'
+            />
+          </label>
+        </div>
+        <div class="form-item">
+          <p class="error-message"></p>
+          <label>Do you agree to our TOS?
+            <input 
+              checked={values.tos}
+              onChange={onChange}
+              name='tos'
+              type='checkbox'
+            />
+          </label>
+        </div>
       </form>
     </div>
   )
